98680fcc3c109b722eae42aa9d6019c4d1526913
LuK1337 <priv.luk@gmail.com>
2017-09-05 19:53:26 +0200

    taoshan: Build our MultiHAL sensors HAL
    Change-Id: I32172a0a6f5b3bf15ead878e91c2edd25b6f1625

--- product/qcom-sensors.mk
+++ product/qcom-sensors.mk
@@ -3,7 +3,12 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
     frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
     frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml
+    
+# Multi HAL conf
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sensors/_hals.conf:system/vendor/etc/sensors/_hals.conf
 
 # Sensors HIDL interfaces
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl
+    android.hardware.sensors@1.0-impl \
+    sensors.msm8960
