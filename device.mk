c4e5af32fc98e140b17d9f9cd3e3bf38e8d0e19c
Adrian DC <radian.dc@gmail.com>
2017-12-06 20:51:11 +0100

    build: Use Android Go configurations
    Change-Id: I877caf945d6ca8556494a0b623f383ea30b28c8b
    Signed-off-by: Adrian DC <radian.dc@gmail.com>

--- device.mk
+++ device.mk
@@ -25,6 +25,9 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 # Product common configurations
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+# Inherit common Android Go configurations
+$(call inherit-product, build/target/product/go_defaults.mk)
+
 # Dalvik heap configurations
 $(call inherit-product-if-exists, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
 
