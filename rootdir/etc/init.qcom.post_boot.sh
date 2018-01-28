1f64e1bf82128eefedfad32fc9647e35ced453f1
Avinaba Dalal <d97.avinaba@gmail.com>
2018-01-28 19:28:40 +0530

    taoshan: Completely remove mpdecision

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -182,27 +182,11 @@ case "$target" in
          echo 20000 > /sys/module/msm_dcvs/cores/gpu0/slack_time_max_us
          echo 20000 > /sys/module/msm_dcvs/cores/gpu0/slack_time_min_us
          echo 0 > /sys/module/msm_dcvs/cores/gpu0/slack_mode_dynamic
-         # set msm_mpdecision parameters
-         echo 45000 > /sys/module/msm_mpdecision/slack_time_max_us
-         echo 15000 > /sys/module/msm_mpdecision/slack_time_min_us
-         echo 100000 > /sys/module/msm_mpdecision/em_win_size_min_us
-         echo 1000000 > /sys/module/msm_mpdecision/em_win_size_max_us
-         echo 3 > /sys/module/msm_mpdecision/online_util_pct_min
-         echo 25 > /sys/module/msm_mpdecision/online_util_pct_max
-         echo 97 > /sys/module/msm_mpdecision/em_max_util_pct
-         echo 2 > /sys/module/msm_mpdecision/rq_avg_poll_ms
-         echo 10 > /sys/module/msm_mpdecision/mp_em_rounding_point_min
-         echo 85 > /sys/module/msm_mpdecision/mp_em_rounding_point_max
-         echo 50 > /sys/module/msm_mpdecision/iowait_threshold_pct
          #set permissions for the nodes needed by display on/off hook
          chown system /sys/module/msm_dcvs/cores/cpu0/slack_time_max_us
          chown system /sys/module/msm_dcvs/cores/cpu0/slack_time_min_us
-         chown system /sys/module/msm_mpdecision/slack_time_max_us
-         chown system /sys/module/msm_mpdecision/slack_time_min_us
          chmod 664 /sys/module/msm_dcvs/cores/cpu0/slack_time_max_us
          chmod 664 /sys/module/msm_dcvs/cores/cpu0/slack_time_min_us
-         chmod 664 /sys/module/msm_mpdecision/slack_time_max_us
-         chmod 664 /sys/module/msm_mpdecision/slack_time_min_us
          soc_id=`cat /sys/devices/system/soc/soc0/id`
          case "$soc_id" in
              "130")
@@ -326,27 +310,6 @@ case "$target" in
     "msm7627a")
         echo 10 > /sys/devices/platform/msm_sdcc.1/idle_timeout
         ;;
-esac
-
-# Post-setup services
-case "$target" in
-    "msm8660" | "msm8960" | "msm8974")
-	rm /data/mpdecision/power_collapse
-	rm /data/mpdecision/standalone_power_collapse
-	rm -rf /data/mpdecision
-	mkdir -p /data/mpdecision
-	chown root.system /data/mpdecision
-	chmod 0770 /data/mpdecision
-        start mpdecision
-    ;;
-    "msm7627a")
-        soc_id=`cat /sys/devices/system/soc/soc0/id`
-        case "$soc_id" in
-            "127" | "128" | "129")
-                start mpdecision
-        ;;
-        esac
-    ;;
 esac
 
 # Enable Power modes and set the CPU Freq Sampling rates
