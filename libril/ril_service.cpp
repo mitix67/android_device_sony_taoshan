6568a8d5e66f85886f74243b02a8368041430b03
Avinaba Dalal <d97.avinaba@gmail.com>
2018-05-18 15:21:18 +0530

    libril: Do not enforce checks on data registration state response length
    libril expects the response length of the result of DATA_REGISTRATION_STATE vendor call as either 6*sizeof(char*) or 11*sizeof(char*). If the the response length is not one of them, it treats it as an invalid response. In case of taoshan, its vendor RIL returns 12(*sizeof(char*)) as result of this call, which makes libril treat it as invalid respone, thus causing data registration failure.
    
    Thus, make this check permissive, and make libril do not treat the response as invalid in such cases. Also, such checks where not done in Nougat libril.
    This change fixes data registration and hence mobile data.
    
    Signed-off-by: Avinaba Dalal <d97.avinaba@gmail.com>

--- libril/ril_service.cpp
+++ libril/ril_service.cpp
@@ -3852,17 +3852,15 @@ int radio::getDataRegistrationStateResponse(int slotId,
         } else if (s_vendorFunctions->version <= 14) {
             int numStrings = responseLen / sizeof(char *);
             if ((numStrings != 6) && (numStrings != 11)) {
-                RLOGE("getDataRegistrationStateResponse Invalid response: NULL");
-                if (e == RIL_E_SUCCESS) responseInfo.error = RadioError::INVALID_RESPONSE;
-            } else {
-                char **resp = (char **) response;
-                dataRegResponse.regState = (RegState) ATOI_NULL_HANDLED_DEF(resp[0], 4);
-                dataRegResponse.rat =  ATOI_NULL_HANDLED_DEF(resp[3], 0);
-                dataRegResponse.reasonDataDenied =  ATOI_NULL_HANDLED(resp[4]);
-                dataRegResponse.maxDataCalls =  ATOI_NULL_HANDLED_DEF(resp[5], 1);
-                fillCellIdentityFromDataRegStateResponseString(dataRegResponse.cellIdentity,
-                        numStrings, resp);
+                RLOGW("getDataRegistrationStateResponse Possible invalid response? Expected numStrings to be 6 or 11, got %d", numStrings);
             }
+            char **resp = (char **) response;
+            dataRegResponse.regState = (RegState) ATOI_NULL_HANDLED_DEF(resp[0], 4);
+            dataRegResponse.rat =  ATOI_NULL_HANDLED_DEF(resp[3], 0);
+            dataRegResponse.reasonDataDenied =  ATOI_NULL_HANDLED(resp[4]);
+            dataRegResponse.maxDataCalls =  ATOI_NULL_HANDLED_DEF(resp[5], 1);
+            fillCellIdentityFromDataRegStateResponseString(dataRegResponse.cellIdentity,
+                    numStrings, resp);
         } else {
             RIL_DataRegistrationStateResponse *dataRegState =
                     (RIL_DataRegistrationStateResponse *)response;
